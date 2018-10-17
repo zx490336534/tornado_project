972ead193ea17e97f5a70c121510f50149a59709
zhongxin <490336534@qq.com>
2018-10-17 19:00:33 +0800

    room页面接口调用

--- handlers/chat.py
+++ handlers/chat.py
@@ -5,26 +5,47 @@ import uuid
 
 import tornado.web
 import tornado.websocket
+import tornado.httpclient
 from .main import AuthBaseHandler
 import tornado.escape
+from tornado.ioloop import IOLoop
+from pycket.session import SessionMixin
+
+
+def make_chat(msg, img_url=None):
+    """
+    生成一个用来格式化 message.html的dict
+    :param msg:
+    :param img:
+    :return:
+    """
+    ret = {
+        'id': str(uuid.uuid4()),
+        'body': msg,
+        'img_url': img_url,
+    }
+    return ret
 
 
 class RoomHanlder(AuthBaseHandler):
     """
     聊天室页面
     """
-
+    @tornado.web.authenticated
     def get(self, *args, **kwargs):
-        self.render('room.html',messages=ChatSocketHandler.cache)
+        self.render('room.html', messages=ChatSocketHandler.cache)
 
 
-class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+class ChatSocketHandler(tornado.websocket.WebSocketHandler, SessionMixin):
     """
     处理响应 websocker 连接
     """
-    waiters = set()     #等待接收信息的用户
-    cache = []          #存放消息
-    cache_size = 200    #消息列表的大小
+    waiters = set()  # 等待接收信息的用户
+    cache = []  # 存放消息
+    cache_size = 200  # 消息列表的大小
+
+    def get_current_user(self):
+        return self.session.get('tudo_user_info')
 
     def open(self, *args, **kwargs):
         """新的WebSocket连接打开，自动调用"""
@@ -37,14 +58,14 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
         ChatSocketHandler.waiters.remove(self)
 
     @classmethod
-    def update_cache(cls,chat):
+    def update_cache(cls, chat):
         """更新消息列表，加入新的消息"""
         cls.cache.append(chat)
         if len(cls.cache) > cls.cache_size:
             cls.cache = cls.cache[-cls.cache_size:]
 
     @classmethod
-    def send_updates(cls,chat):
+    def send_updates(cls, chat):
         """给每个等待接收的用户发送新的消息"""
         for waiter in cls.waiters:
             waiter.write_message(chat)
@@ -53,10 +74,18 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
         """WebSocket 服务端接收到消息，自动调用"""
         print('got message :%s' % message)
         parese = tornado.escape.json_decode(message)
-        chat = {
-            'id':str(uuid.uuid4()),
-            'body':parese['body'],
-        }
-        chat['html'] = tornado.escape.to_basestring(self.render_string('message.html',message=chat))
-        ChatSocketHandler.update_cache(chat)
-        ChatSocketHandler.send_updates(chat)
+        if parese['body'] and parese['body'].startswith('http://'):
+            # url = 'http://source.unsplash.com/random'
+            url = parese['body']
+            client = tornado.httpclient.AsyncHTTPClient()
+            # http://123.56.13.233:8000/save?url=http://source.unsplash.com/random&user=zx&from=room
+            save_api_url = f"http://127.0.0.1:8000/save?url={url}&user={self.current_user}&from=room"
+            IOLoop.current().spawn_callback(client.fetch, save_api_url)
+            chat = make_chat(f'user {self.current_user},url({url}) is processing.')
+            chat['html'] = tornado.escape.to_basestring(self.render_string('message.html', message=chat))
+            self.write_message(chat)
+        else:
+            chat = make_chat(f"[{self.current_user}]:{parese['body']}")
+            chat['html'] = tornado.escape.to_basestring(self.render_string('message.html', message=chat))
+            ChatSocketHandler.update_cache(chat)
+            ChatSocketHandler.send_updates(chat)
--- handlers/service.py
+++ handlers/service.py
@@ -1,11 +1,14 @@
 # coding=utf-8
 # Auth:zx
 # Time:2018/10/14 0014 10:20
+import uuid
 from datetime import datetime
 import tornado.httpclient
 import tornado.web
 import tornado.gen
+import tornado.escape
 from .main import AuthBaseHandler
+from .chat import ChatSocketHandler,make_chat
 from utils.photo import ImageSave
 from utils.account import add_post
 
@@ -15,7 +18,7 @@ class AsyncURLSaveHandler(AuthBaseHandler):
     实现保存指定URL的图片功能.异步版本
     """
 
-    @tornado.web.authenticated
+    # @tornado.web.authenticated
     @tornado.gen.coroutine
     def get(self, *args, **kwargs):
         resp = yield self.fetch_image()
@@ -27,17 +30,25 @@ class AsyncURLSaveHandler(AuthBaseHandler):
         img_saver = ImageSave(self.settings['static_path'], 'x.jpg')
         img_saver.save_upload(resp.body)
         img_saver.make_thumb()
-        post = add_post(self.current_user, img_saver.upload_url, img_saver.thumb_url)
-        print(f'--  {datetime.now()} -end fetch:#{post.id}')
-
-        self.redirect('/post/{}'.format(post.id))
+        user = self.get_argument('user','')
+        if user:
+            post = add_post(user, img_saver.upload_url, img_saver.thumb_url)
+            print(f'--  {datetime.now()} -end fetch:#{post.id}')
+            chat = make_chat(f'-----[{user}]-----',img_saver.thumb_url)
+            chat['post_id'] = post.id
+            chat['html'] = tornado.escape.to_basestring(self.render_string('message.html', message=chat))
+            ChatSocketHandler.update_cache(chat)
+            ChatSocketHandler.send_updates(chat)
+        else:
+            self.write('user error')
+        # self.redirect('/post/{}'.format(post.id))
 
     @tornado.gen.coroutine
     def fetch_image(self):
         url = self.get_argument('url', '')
         client = tornado.httpclient.AsyncHTTPClient()
         print(f'-- {datetime.now()} -going to fetch:{url}')
-        yield tornado.gen.sleep(5)
+        # yield tornado.gen.sleep(5)
         resp = yield client.fetch(url)
         return resp
 
